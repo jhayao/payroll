ae895c39207b1c78b2ba497ca7528a7bfdd195b0
junriel hayao <liernuj25@gmail.com>
2026-01-22 13:48:01 +0800

    aaa

--- resources/views/payroll/report/summary.blade.php
+++ resources/views/payroll/report/summary.blade.php
@@ -1,73 +1,87 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Summary of Dates</title>
     <style>
         body {
             font-family: 'DejaVu Sans', sans-serif;
             font-size: 9px;
+            margin: 0;
+            padding: 10px;
         }
+
         table {
             width: 100%;
             border-collapse: collapse;
             table-layout: fixed;
         }
-        th, td {
+
+        th,
+        td {
             border: 1px solid #000;
-            padding: 2px;
+            padding: 4px 2px;
             text-align: center;
-            overflow: hidden;
             word-wrap: break-word;
         }
+
         th {
             background-color: #f2f2f2;
             font-weight: bold;
-            font-size: 8px; /* Smaller header font */
+            font-size: 8px;
+            text-transform: uppercase;
         }
+
         .text-left {
             text-align: left;
-            padding-left: 4px;
+            padding-left: 5px;
         }
-        .header {
-            text-align: center;
-            margin-bottom: 20px;
+
+        /* Column Widths */
+        .name-col {
+            width: 120px;
         }
-        .name-col { width: 120px; }
-        .pos-col { width: 80px; }
-        .total-col { width: 45px; }
-        .date-col { width: auto; font-size: 8px; }
-        .am-pm-col { width: 20px; font-size: 7px; background-color: #f9f9f9; }
-        .vertical-text {
-            writing-mode: vertical-rl;
-            transform: rotate(180deg);
-            white-space: nowrap;
+
+        .pos-col {
+            width: 80px;
+        }
+
+        .total-col {
+            width: 45px;
+        }
+
+        .date-col {
+            font-size: 8px;
+        }
+
+        /* Cell States */
+        .active-cell {
+            background-color: #e6f3ff;
+        }
+
+        .bold {
+            font-weight: bold;
         }
     </style>
 </head>
+
 <body>
+
     <table>
         <thead>
             <tr>
-                <th rowspan="2" class="name-col">NAME</th>
-                <th rowspan="2" class="pos-col">POSITION</th>
-                <th colspan="{{ $dateCounts }}">SUMMARY OF DATES</th>
+                <th rowspan="2" class="name-col">Name</th>
+                <th rowspan="2" class="pos-col">Position</th>
+                <th colspan="{{ count($dates) * 2 }}">Summary of Dates</th>
                 <th rowspan="2" class="total-col">Total<br>Days</th>
-                <th rowspan="2" class="total-col">Total<br>Overtime</th>
-                <th rowspan="2" class="total-col">Total<br>Undertime</th>
+                <th rowspan="2" class="total-col">Total<br>OT</th>
+                <th rowspan="2" class="total-col">Total<br>UT</th>
             </tr>
             <tr>
                 @foreach($dates as $date)
-                    <th class="date-col" colspan="2">{{ $date->format('m/d/Y') }}</th>
+                    <th class="date-col" colspan="2">{{ $date->format('m/d') }}</th>
                 @endforeach
-            </tr>
-            <tr>
-                <th class="name-col"></th>
-                <th class="pos-col"></th>
-                <th class="date-col"></th>
-                <th class="total-col"></th>
-                <th class="total-col"></th>
-                <th class="total-col"></th>
             </tr>
         </thead>
         <tbody>
@@ -75,25 +89,27 @@
                 <tr>
                     <td class="text-left">{{ strtoupper($row['name']) }}</td>
                     <td>{{ strtoupper($row['position']) }}</td>
+
                     @foreach($dates as $date)
                         @php 
-                            $dayData = $row['days'][$date->format('Y-m-d')];
-                            $am = $dayData['am'];
-                            $pm = $dayData['pm'];
+                            $day = $row['days'][$date->format('Y-m-d')] ?? ['am' => 0, 'pm' => 0];
                         @endphp
-                        <td style="background-color: {{ $am > 0 ? '#e6f3ff' : 'transparent' }}">
-                           {{ $am == 1 ? '1' : '0' }}
+
+                        <td class="{{ $day['am'] > 0 ? 'active-cell' : '' }}">
+                            {{ $day['am'] == 1 ? '1' : '0' }}
                         </td>
-                        <td style="background-color: {{ $pm > 0 ? '#e6f3ff' : 'transparent' }}">
-                           {{ $pm == 1 ? '1' : '0' }}
-                        </td>
+                        <td class="{{ $day['pm'] > 0 ? 'active-cell' : '' }}">
+                                {{ $day['pm'] == 1 ? '1' : '0' }}
+                                </td>
                     @endforeach
-                    <td><strong>{{ $row['total_days'] }}</strong></td>
+                    <td class="bold">{{ $row['total_days'] }}</td>
                     <td>{{ $row['total_ot'] }}</td>
-                    <td>{{ $row['total_undertime'] }}</td>
-                </tr>
+                        <td>{{ $row['total_undertime'] }}</td>
+                    </tr>
             @endforeach
         </tbody>
-    </table>
+    </t
+able>
+
 </body>
 </html>
